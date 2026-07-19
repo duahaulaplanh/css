d908407bafd5a7b02af00beaae3ea4ee5341e2f4
duahaulaplanh <hoangnhh2408@gmail.com>
2026-07-19 18:30:05 +0700

    Add files via upload

---
+++ Javascript/week1.js
@@ -0,0 +1,12 @@
+function hienanh1() {
+    document.getElementById('text').innerHTML = "Đây là hình ảnh 1";
+    document.getElementById('anh').src = "img/1.jpg";
+}
+function hienanh2() {
+    document.getElementById('text').innerHTML = "Còn đây là ảnh 2";
+    document.getElementById('anh').src = "img/2.jpg";
+}
+function hienanh3() {
+    document.getElementById('text').innerHTML = "Tất nhiên còn đây là ảnh 3=))";
+    document.getElementById('anh').src = "img/3.png";
+}
